530fdf8defbf4202d628042406d84820f56af6a4
robertordz14 <hroberto.rdz@gmail.com>
2022-12-05 10:03:03 -0600

    sw

--- sw.js
+++ sw.js
@@ -1,71 +1,45 @@
-//asignar nombre y version de cache
-const CACHE_NAME = "v1_cache_awp";
-
-//configurar los archivos del cache
-
-var urlToCache = [
-  "./", 
-  "./css/style.css", 
-  "./images/facebook.png",
-  "./images/instagram.png",
-  "./images/twiter.png",
-  "./images/git.png",
-  "./images/img1.png",
-  "./images/img2.png",
-  "./images/img3.png",
-  "./images/logo.png",
-  "./images/landscape.png"
-];
-
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlToCache).then(() => {
-          self.skipWaiting();
-        });
-      })
-      .catch((err) => {
-        console.log("No se ha registrado el cache", err);
-      })
-  );
+self.addEventListener('install', e =>{
+  const cacheProm = caches.open('cache-v1')
+      .then(cache => {
+          return cache.addAll([
+            "./", 
+            "/css/style.css", 
+            '/main.js',
+            "/images/facebook.png",
+            "/images/instagram.png",
+            "/images/twiter.png",
+            "/images/git.png",
+            "/images/img1.png",
+            "/images/img2.png",
+            "/images/img3.png",
+            "/images/logo.png",
+            "/images/landscape.jpg",
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+          ])
+          
+      });
+  e.waitUntil(cacheProm);
 });
 
-//Evento activate
-self.addEventListener("activate", (e) => {
-  const cacheWhiteList = [CACHE_NAME];
-  e.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return Promise.all(
-          cacheNames.map((cacheName) => {
-            if (cacheWhiteList.indexOf(cacheName) === -1) {
-                // Borrar los elementos q no se necesitan
-                return caches.delete(cacheName);
-              }
-            })
-          );
-        })
-        .then(() => {
-          //activar la cache
-          self.clients.claim();
-        })
-    );
-  });
-  
-  //Evento fetch
-  
-  self.addEventListener("fetch", (e) => {
-    e.respondWith(
-      caches.match(e.request).then((res) => {
-        if (res) {
-          //devuelvo los datos desde cache
-          return res;
-        }
-        return fetch(e.request);
-  })
-  );
-  });
+self.addEventListener('fetch', e =>{
+  //cache with network fallback
+  const respuesta = caches.match( e.request )
+      .then ( res => {
+          if ( res ) return res;
+          //no existe el archivo
+          //tengo que ir a la web
+          console.log('No existe', e.request.url);
+          return fetch( e.request ).then ( newResp => {
+              caches.open('cache-v1')
+                  .then( cache => {
+                      cache.put( e.request, newResp);
+                  }
+
+                  )
+              return newResp.clone;
+          });
+      });
+      e.respondWith(respuesta);
+  //only cache
+  //e.respondWith( caches.match(e.request));
+});
